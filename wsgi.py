af2acb7384e17622589b2c55977e52ff4f317ef0
Billy Tat <billyt@activestate.com>
2014-08-12 12:08:18 -0700

    Use non-legacy buildpack

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,10 @@
 from pyramid.config import Configurator
 from pyramid.response import Response
 from paste.httpserver import serve
-
+import os
 
 def hello_world(request):
     return Response('Hello world!')
-
 
 def goodbye_world(request):
     return Response('Goodbye world!')
@@ -14,4 +13,5 @@ config = Configurator()
 config.add_view(hello_world)
 config.add_view(goodbye_world, name='goodbye')
 application = config.make_wsgi_app()
-# serve(app, host='0.0.0.0')
+port = os.environ['PORT']
+serve(application, host='0.0.0.0', port=port)
